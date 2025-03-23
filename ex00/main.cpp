13d5c481a273123ab427655ce01aa6c26b3e95b3
chemshoflack8@gmail.com <chemshoflack8@gmail.com>
2025-03-23 15:19:13 +0100

    end of he exercice

---
+++ ex00/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chhoflac <chhoflac@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/23 15:02:45 by chhoflac          #+#    #+#             */
+/*   Updated: 2025/03/23 15:17:37 by chhoflac         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int main(void){
+    Fixed a;
+    
+    
+    std::cout << "Raw value with default constructor : " << a.getRawBits() << std::endl;
+    a.setRawBits(2);
+    std::cout << "Raw value after setting : " << a.getRawBits() << std::endl;
+    Fixed b = a;
+    std::cout << "Raw value of b after using copy constructor : " << b.getRawBits() << std::endl;
+    Fixed c;
+    c = a;
+    std::cout << "Raw value aftr using copy assignment operator : " << c.getRawBits() << std::endl;
+
+}
