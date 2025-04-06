a11c2fe562005577abbdcadfc5e4b6752ef6b4df
chemshoflack8@gmail.com <chemshoflack8@gmail.com>
2025-04-06 16:02:19 +0200

    exo2 done, just have to make the test (dont forget int min and max test and is it necessary?

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,12 +6,23 @@
 /*   By: chhoflac <chhoflac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/04 11:28:36 by chhoflac          #+#    #+#             */
-/*   Updated: 2025/04/04 11:34:02 by chhoflac         ###   ########.fr       */
+/*   Updated: 2025/04/06 15:46:12 by chhoflac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
 int main(void){
-	return (0);
+	Fixed a;
+    Fixed const b(Fixed(5.05f) * Fixed(2));
+
+    std::cout << a << std::endl;
+    std::cout << ++a << std::endl;
+    std::cout << a << std::endl;
+    std::cout << a++ << std::endl;
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+    std::cout << Fixed::max(a, b) << std::endl;
+
+    return (0);
 }
