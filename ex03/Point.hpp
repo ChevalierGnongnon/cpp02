7efc87bc8d059a7df15d47aade672cc997b796da
chhoflac <chhoflac@student.42.fr>
2025-04-08 10:37:10 +0200

    Point.hpp update

--- ex03/Point.hpp
+++ ex03/Point.hpp
@@ -6,7 +6,7 @@
 /*   By: chhoflac <chhoflac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/06 17:24:52 by chhoflac          #+#    #+#             */
-/*   Updated: 2025/04/07 15:37:16 by chhoflac         ###   ########.fr       */
+/*   Updated: 2025/04/08 10:35:46 by chhoflac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,20 @@
 
 class Point{
     private:
-		Fixed pos_x;
-		Fixed pos_y;
+		const Fixed pos_x;
+		const Fixed pos_y;
     public:
         Point();
         Point(Fixed pos_x, Fixed pos_y);
         Point(const Point &src);
-        Point &operator = (const Point &otherOne);
         ~Point();
 		
         Fixed	get_posX() const;
         Fixed	get_posY() const;
-		void	set_posX(const Fixed &x);
-		void	set_posY(const Fixed &y);
+		
 		
 };
-bool	bsp(Point &a, Point &b, Point &c, Point &p);
+
+bool bsp(Point const a, Point const b, Point const c, Point const p);
 
 #endif
